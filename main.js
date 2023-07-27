85eb15d7c4b1e686fb11b9afed3833ec7cf2bfe5
jay <jayrajshakha01@gmail.com>
2023-07-27 18:20:05 +0530

    All Done

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
+const bitcoin = document.querySelector("#bt-price");
+const ethereumCoin = document.querySelector("#eth-price");
+const dogeCoin = document.querySelector("#doge-price");
 
-console.log('(---------------------------------');
+const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Cethereum&vs_currencies=usd";
+
+// inr url use for indin rupees 
+
+//const inr = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Cethereum&vs_currencies=inr";
+
+function price(){
+
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+
+       bitcoin.textContent = `$${item.bitcoin.usd}`;
+       ethereumCoin.textContent = `$${item.ethereum.usd}`;
+       dogeCoin.textContent = `$${item.dogecoin.usd}`;
+       
+    })
+    .catch(err => console.log(err));
+}
+
+  price();
